ff14c9effd7664ca8b51a4c1c11f432e579c6847
Md.Ashraful Islam <crackthecode10@gmail.com>
2024-05-16 04:26:26 +0600

    Seperate from Year-2024 repository for deploying it on render.com

---
+++ client/src/pages/Projects.jsx
@@ -0,0 +1,16 @@
+import CallToAction from '../components/CallToAction';
+
+export default function Projects() {
+  return (
+    <div className='min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3'>
+      <h1 className='text-3xl font-semibold'>Pojects</h1>
+      <p className='text-md text-gray-500'>
+        Build fun and engaging projects while learning HTML, CSS, JavaScript, React, and Node.js. Each project is designed to challenge and enhance your coding skills, helping you become a more proficient developer.
+      </p>
+      <blockquote className='text-md text-gray-600 italic'>
+        "The best way to learn is by doing. Dive into these projects and discover the joy of creating something from scratch."
+      </blockquote>
+      <CallToAction />
+    </div>
+  )
+}
